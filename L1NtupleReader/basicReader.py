1edb0d799b0c8f665bc0f0824c3abf7042bd101e
Jona Motta <jona.motta@polytechnique.edu>
2022-04-11 11:57:52 +0200

    make jet-tower association and find central tower

--- L1NtupleReader/basicReader.py
+++ L1NtupleReader/basicReader.py
@@ -1,16 +1,15 @@
-import os
-import sys
-import glob
-import csv
-import numpy as np
-import uproot3
-import pandas as pd
-import argparse
 from sklearn.model_selection import train_test_split
-
-from TowerGeometry import FindEtaGap
-from TowerGeometry import FindPhiGap
-
+from TowerGeometry import FindIeta
+from TowerGeometry import FindIphi
+from itertools import chain
+import pandas as pd
+import numpy as np
+import argparse
+import uproot3
+import glob
+import sys
+import csv
+import os
 
 class Logger(object):
     def __init__(self, file):
@@ -29,6 +28,14 @@ class Logger(object):
 
 def InChunkyDonut(i_eta, i_phi, jet_EtaGap, jet_PhiGap):
     return np.abs(i_eta - jet_EtaGap) <= 4 and np.abs(i_phi - jet_PhiGap) <= 4
+
+
+def deltarSelect( df, dRcut ):
+    deta = np.abs(df['jetEta'] - df['jetEta_joined'])
+    dphi = np.abs(df['jetPhi'] - df['jetPhi_joined'])
+    sel = dphi > np.pi
+    dphi = np.abs(sel*(2*np.pi) - dphi)
+    return (np.sqrt(dphi*dphi+deta*deta) > dRcut) | ((deta == 0) & (dphi == 0))
 
 #######################################################################
 ######################### SCRIPT BODY #################################
@@ -66,17 +73,18 @@ if __name__ == "__main__" :
     # list Ntuples
     InFiles = []
     for folder_name in folder_names:
-        subfolders = glob.glob(indir+'/'+folder_name+'/Ntuple*')
+        subfolders = glob.glob(indir+'/'+folder_name+'/Ntuple*.root')
         for subfolder in subfolders:
             InFiles.append(indir+'/'+folder_name+'/'+subfolder)
-    # print(len(InFiles))
+    #print(len(InFiles))
+
 
     testInfile = indir+'/SinglePhoton_Pt-0To200-gun__Run3Summer21DR-NoPUFEVT_120X_mcRun3_2021_realistic_v6-v2__reEmulated_appliedHCALpfa1p/Ntuple_9.root'
-    # testOutfile = outdir+'/test.hdf5'
-    testOutfile = outdir+'/test.csv'
+    testOutHDF = outdir+'/test.hdf5'
+    testOutCSV = outdir+'/test.csv'
 
     fieldnames = ['ev','i_eta','i_phi','i_em','i_had','i_et']
-    with open(testOutfile, 'w', newline='') as csvfile:
+    with open(testOutCSV, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
@@ -103,24 +111,128 @@ if __name__ == "__main__" :
 
     dfET = pd.concat([dfE, dfT], axis=1)
     dfEJ = pd.concat([dfE, dfJ], axis=1)
-    dfET.set_index(b"event", inplace=True)
-    dfEJ.set_index(b"event", inplace=True)
 
-    print(dfET.shape[0])
-    print(dfEJ.shape[0])
+    ## DEBUG
+    dfET = dfET.head(1)
+    dfEJ = dfEJ.head(1)
+    
+    # flatten out the dataframes so that ech entry of the dataframe is a number and not a vector
+    dfFlatET = pd.DataFrame({
+        'event': np.repeat(dfET[b'event'].values, dfET[b'ieta'].str.len()), # event IDs are copied to keep proper track of what is what
+        'ieta': list(chain.from_iterable(dfET[b'ieta'])),
+        'iphi': list(chain.from_iterable(dfET[b'iphi'])),
+        'iem' : list(chain.from_iterable(dfET[b'iem'])),
+        'ihad': list(chain.from_iterable(dfET[b'ihad'])),
+        'iet' : list(chain.from_iterable(dfET[b'iet']))
+        })
 
-# Is it too much to have everything compressed in one pandas? In this way we can easily connect each jet with its towers
-    dfETJ = pd.concat([dfE, dfT, dfJ], axis=1)
-    dfETJ.set_index(b"event", inplace=True)
-    dfETJ = dfETJ.sort_values(by=[b'event'])
-    dfETJ.reset_index(level=0,drop=False)
+    dfFlatEJ = pd.DataFrame({
+        'event': np.repeat(dfEJ[b'event'].values, dfEJ[b'jetEta'].str.len()), # event IDs are copied to keep proper track of what is what
+        'jetEta': list(chain.from_iterable(dfEJ[b'jetEta'])),
+        'jetPhi': list(chain.from_iterable(dfEJ[b'jetPhi'])),
+        'jetPt' : list(chain.from_iterable(dfEJ[b'jetPt']))
+        })
+    dfFlatEJ['jetId'] = dfFlatEJ.index # each jet gets an identifier based on a progressive value independent of event -> this allows further flexibility of ID on top of event
 
-    for i in range(dfETJ.shape[0]):
-        jet_EtaGap = FindEtaGap(dfETJ[b'jetEta'].iloc[i])
-        jet_PhiGap = FindPhiGap(dfETJ[b'jetPhi'].iloc[i])
-        for i_eta, i_phi, i_em, i_had, i_et in zip(dfETJ[b'ieta'].iloc[i], dfETJ[b'iphi'].iloc[i],dfETJ[b'iem'].iloc[i],dfETJ[b'ihad'].iloc[i],dfETJ[b'iet'].iloc[i]):
-            if InChunkyDonut(i_eta, i_phi, jet_EtaGap, jet_PhiGap):
-                print(i,i_eta,i_phi,i_em,i_had,i_et)
-                with open(testOutfile, 'a', newline='') as csvfile:
-                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                    writer.writerow({'ev':i, 'i_eta':i_eta, 'i_phi':i_phi, 'i_em':i_em, 'i_had':i_had, 'i_et':i_et})
+    # reset indeces to be the event number to be able to join the SFs later
+    dfFlatET.set_index('event', inplace=True)
+    dfFlatEJ.set_index('event', inplace=True)
+
+    ## DEBUG
+    # print(dfET.shape[0])
+    # print(dfEJ.shape[0])
+
+    # cerate all teh possible combinations of jets per each event
+    dfFlatEJ  = dfFlatEJ.join(dfFlatEJ, on='event', how='left', rsuffix='_joined', sort=False)
+    # select only those jets that are at least dRcut away from each other
+    dfFlatEJ['dRsafe'] = deltarSelect(dfFlatEJ, 0.5)
+    notSafe = list(dfFlatEJ[(dfFlatEJ['dRsafe']==False)]['jetId'])
+    dfFlatEJ = dfFlatEJ[dfFlatEJ.jetId.isin(notSafe) == False]
+    dfFlatEJ.drop(['jetEta_joined', 'jetPhi_joined', 'jetPt_joined', 'jetId_joined', 'dRsafe'], axis=1, inplace=True) # drop columns not needed anymore
+    dfFlatEJ.drop_duplicates('jetId', keep='first', inplace=True) # drop duplicates of teh jets
+
+    # find ieta/iphi values for the jets
+    FindIeta_vctd = np.vectorize(FindIeta)
+    FindIphi_vctd = np.vectorize(FindIphi)
+    dfFlatEJ['jetIeta'] = FindIeta_vctd(dfFlatEJ['jetEta'])
+    dfFlatEJ['jetIphi'] = FindIphi_vctd(dfFlatEJ['jetPhi'])
+    dfFlatEJ.drop(['jetEta', 'jetPhi'], axis=1, inplace=True) # drop columns not needed anymore
+
+    # join the jet and the towers datasets -> this creates all the possible combination of towers and jets for each event
+    dfFlatEJT  = dfFlatEJ.join(dfFlatET, on='event', how='left', rsuffix='_joined', sort=False)
+
+    # identify the central tower for each jet by looking for the tower with the same ieta/iphi as the jet
+    # FIXME - THIS NEEDS TO BE CHECKED!!! 
+    #  --> in the sense that after having fixed the FindIeta/FindIphi function we need to check if it really works
+    #  --> at a first look I would say that at least for ieta away from 29 it works
+    dfFlatEJT['central'] = (dfFlatEJT['jetIeta']==dfFlatEJT['ieta']) & (dfFlatEJT['jetIphi']==dfFlatEJT['iphi']) 
+
+    # DEBUG
+    print(dfFlatEJT)
+
+    # this is just a sort of example of how we would like to have the dataframe looking at the end
+    # we have a column per each tower in the chunky donut --> this is the meaning of one hot encoding
+    for i in range(81):
+        dfFlatEJT['iTinCD'+str(i)] = np.zeros(dfFlatEJT.shape[0])
+
+    
+    # save hdf5 file
+    store = pd.HDFStore(testOutHDF, mode='w')
+    store = dfFlatEJT
+    #store.close()
+
+
+    # make the produced files accessible to the other people otherwise we cannot work together
+    os.system('chmod -R 764 '+testOutHDF)
+    os.system('chmod -R 764 '+testOutCSV)
+
+
+
+    ## IM LEAVING THIS PART OF THE CODE THAT YOU WROTE ELENA, CAUSE MAYBE YOU WANT TO REUSE SOMETHING OR AT LEAST RESATRT FROM WERE YOU LEFT
+    ## AS I MENTIONED IN ONE OF UR CHATS, REMEMBER THAT LOOPING OVER PANDAS DATAFRAMES IS EXTREMELY TIME CONSUMING 
+    ##      --> TRY TO ONLY USE VECTORIZED OPERATIONS OR WORST CASE AN APPLY FCT
+    ##      --> REMEMBRER THAT SIOMETIMES APPLY IS EVEN SLOWER THAN LOOPING CAUSE IT TAKES QUITE A BIT OF MEMORY TO CREATE SERIES INSIDE THE APPLY LOOP)
+
+    # # Is it too much to have everything compressed in one pandas? In this way we can easily connect each jet with its towers
+    # dfETJ = pd.concat([dfE, dfT, dfJ], axis=1)
+    # dfETJ.set_index(b"event", inplace=True)
+    # dfETJ = dfETJ.sort_values(by=[b'event'])
+    # dfETJ.reset_index(level=0,drop=False)
+
+    # for i in range(dfETJ.shape[0]):
+    #     jet_EtaGap = FindEtaGap(dfETJ[b'jetEta'].iloc[i])
+    #     jet_PhiGap = FindPhiGap(dfETJ[b'jetPhi'].iloc[i])
+    #     for i_eta, i_phi, i_em, i_had, i_et in zip(dfETJ[b'ieta'].iloc[i], dfETJ[b'iphi'].iloc[i],dfETJ[b'iem'].iloc[i],dfETJ[b'ihad'].iloc[i],dfETJ[b'iet'].iloc[i]):
+    #         if InChunkyDonut(i_eta, i_phi, jet_EtaGap, jet_PhiGap):
+    #             print(i,i_eta,i_phi,i_em,i_had,i_et)
+    #             with open(testOutCSV, 'a', newline='') as csvfile:
+    #                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    #                 writer.writerow({'ev':i, 'i_eta':i_eta, 'i_phi':i_phi, 'i_em':i_em, 'i_had':i_had, 'i_et':i_et})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
