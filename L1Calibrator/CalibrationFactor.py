68a5f62ac203a95c4ec1568b2621899c7a71892e
Elena Vernazza <e.vernazza1@campus.unimib.it>
2022-04-25 11:59:37 +0200

    Folder definition

--- L1Calibrator/CalibrationFactor.py
+++ L1Calibrator/CalibrationFactor.py
@@ -1,11 +1,15 @@
 #librairies utiles
 import numpy as np
 import os
+import sys
 import copy
 import pandas as pd
 import matplotlib.pyplot as plt
+from alternateModel4ECAL import *
+sys.path.insert(0,'..')
+from L1NtupleReader.TowerGeometry import *
 
-from ModelCMS import *
+eta_towers = list(TowersEta.keys())
 
 # Returns matrix with scale factors for the trained model (couche)
 # The matrix has 40 rows, for all the eta towers, and as many columns as the number of the energy bins
@@ -37,14 +41,19 @@ def ExtractSF (model, bins):
 
     return SF_matrix
 
+
+#######################################################################
+######################### SCRIPT BODY #################################
+#######################################################################
+
 ### To run:
-### python3 CalibrationFactor.py --model data_ECAL/Model_ECAL 
+### python3 CalibrationFactor.py --in /data_CMS/cms/motta/CaloL1calibraton/2022_04_21_NtuplesV1/ECALtraining --v ECAL 
 
 if __name__ == "__main__" :
 
     from optparse import OptionParser
     parser = OptionParser()
-    parser.add_option("--model",    dest="model",   help="Folder with trained model",       default=None)
+    parser.add_option("--in",       dest="indir",   help="Input folder with trained model", default=None)
     parser.add_option("--bins",     dest="bins",    help="Comma separated energy bins",     default=None)
     parser.add_option("--out",      dest="odir",    help="Output folder",                   default=None)
     parser.add_option("--v",        dest="v",       help="Ntuple type ('ECAL' or 'HCAL')",  default='ECAL')
@@ -52,16 +61,15 @@ if __name__ == "__main__" :
     print(options)
 
     # Definition of the trained model
-    if options.model:
-        modeldir = options.model
-    else:
-        modeldir = '/data_CMS/cms/motta/CaloL1calibraton/2022_04_21_NtuplesV1/ECALtraining/model_ECAL'
-    print('\nTrained model = {}\n'.format(modeldir))
+    indir = options.indir
+    modeldir = indir + '/model_' + options.v
+    print('\nModel dir = {}\n'.format(modeldir))
 
     model1 = keras.models.load_model(modeldir + '/model', compile=False)
     couche = keras.models.load_model(modeldir + '/couche', compile=False)
     
     # Definition of energy bins in units of 0.5 GeV, from 1 to 510
+    # It will be optimized by a separated script
     if options.bins:
         bins_energy = options.bins.split(',')
     else:
@@ -72,13 +80,14 @@ if __name__ == "__main__" :
     if options.odir:
         odir = options.odir
     else:
-        odir = os.getcwd() + '/data_ECAL'
+        odir = indir + '/data_' + options.v
     os.system('mkdir -p '+ odir)
-    print('\nOutput folder = {}\n'.format(odir))
+    print('\nOutput dir = {}\n'.format(odir))
 
     # produce scale factors for every bin and every eta tower (matrix 40 * nbins)
     SFOutFile = odir + '/ScaleFactors_' + options.v + '.csv'
     ScaleFactors = ExtractSF(couche, bins_energy)
+
     # Add eta references and save to output csv file
     ScaleFactors_index = np.c_[eta_towers, ScaleFactors]
     head_text = 'ieta'
@@ -86,4 +95,4 @@ if __name__ == "__main__" :
         head_text = head_text + ',{}-{}'.format(bins_energy[i], bins_energy[i+1])
     np.savetxt(SFOutFile, ScaleFactors_index, delimiter=",", header=head_text, fmt=','.join(['%i'] + ['%1.22f']*(len(bins_energy)-1)))
 
-    print('\nScale Factors saved to {}'.format(SFOutFile))
+    print('\nScale Factors saved to: {}'.format(SFOutFile))
