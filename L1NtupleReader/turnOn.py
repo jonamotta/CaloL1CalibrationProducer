d348a9e3d1298b660dce076334aa8426a0b6b96b
Olivier Davignon <davignon@cern.ch>
2022-05-27 00:56:00 +0200

    updated version of the turn on script

--- L1NtupleReader/turnOn.py
+++ L1NtupleReader/turnOn.py
@@ -1,4 +1,5 @@
 from itertools import chain
+from xml.dom.expatbuilder import parseString
 from TowerGeometry import *
 import pandas as pd
 import numpy as np
@@ -10,61 +11,69 @@ import os
 import ROOT
 from array import array
 
-print('cmd entry:', sys.argv)
+#print('cmd entry:', sys.argv)
 
+#reading input parameters
 directory = sys.argv[1]
 nevents = int(sys.argv[2])
 label = sys.argv[3]
 
-#f = ROOT.TFile("/data_CMS/cms/motta/CaloL1calibraton/L1NTuples/QCD_Pt-15to7000_TuneCP5_Flat_13p6TeV-pythia8__Run3Winter22DR-PUForTRK_DIGI_122X_mcRun3_2021_realistic_v9-v2__GEN-SIM-DIGI-RAW_uncalib_applyHCALpfa1p/Ntuple_200.root")
-
-#directory = "/data_CMS/cms/motta/CaloL1calibraton/L1NTuples/QCD_Pt-15to7000_TuneCP5_Flat_13p6TeV-pythia8__Run3Winter22DR-PUForTRK_DIGI_122X_mcRun3_2021_realistic_v9-v2__GEN-SIM-DIGI-RAW_uncalib_applyHCALpfa1p/"
-#nevents = 10000
-
+#defining input trees
 eventTree = ROOT.TChain("l1EventTree/L1EventTree")
 genTree = ROOT.TChain("l1GeneratorTree/L1GenTree")
 emuTree = ROOT.TChain("l1UpgradeEmuTree/L1UpgradeTree")
 
+#reading input files
 eventTree.Add(directory + "/Ntuple*.root")
 genTree.Add(directory + "/Ntuple*.root")
 emuTree.Add(directory + "/Ntuple*.root")
 
+#getting entries
 nEntries = eventTree.GetEntries()
+
+#run on entries specified by usuer, or only on entries available if that is exceeded
 if nevents > nEntries: nevents = nEntries
 print("will process",nevents,"events...")
 
+#defining binning of histogram
 bins = [0, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 100, 120, 150, 180, 250]
 
-empty = ROOT.TH1F("empty","empty",len(bins)-1, array('f',bins))
-pt = ROOT.TH1F("pt","pt",len(bins)-1, array('f',bins))
-passing20 = ROOT.TH1F("passing20","passing20",len(bins)-1, array('f',bins))
-passing35 = ROOT.TH1F("passing35","passing35",len(bins)-1, array('f',bins))
-passing50 = ROOT.TH1F("passing50","passing50",len(bins)-1, array('f',bins))
-passing100 = ROOT.TH1F("passing100","passing100",len(bins)-1, array('f',bins))
-passing150 = ROOT.TH1F("passing150","passing150",len(bins)-1, array('f',bins))
+#list the ET thresholds to be tested
+thresholds = [20, 35, 50, 100, 150]
 
+#passing histograms (numerators)
+passing = []
+for threshold in thresholds:
+        passing.append(ROOT.TH1F("passing"+str(threshold),"passing"+str(threshold),len(bins)-1, array('f',bins)))
+
+#dummy histogram for plotting
+empty = ROOT.TH1F("empty","empty",len(bins)-1, array('f',bins))
+
+#pt spectrum
+pt = ROOT.TH1F("pt","pt",len(bins)-1, array('f',bins))
+
+#denominator
 total = ROOT.TH1F("total","total",len(bins)-1, array('f',bins))
 
-
+#loop on events
 for i in range(0, nevents):
     if i%100==0: print(i)
+    #getting entries
     entry = eventTree.GetEntry(i)
     entry2 = emuTree.GetEntry(i)
     entry3 = genTree.GetEntry(i)
 
     L1_nJets = emuTree.L1Upgrade.nJets
-    #if L1_nJets > 0: print(emuTree.L1Upgrade.jetEt[0])
-
     Gen_nJets = genTree.Generator.nJet
 
+    #loop on generator jets
     for igenJet in range(0,Gen_nJets):
 
         Gen_jet = ROOT.TLorentzVector()
         Gen_jet.SetPtEtaPhiM(genTree.Generator.jetPt[igenJet], genTree.Generator.jetEta[igenJet], genTree.Generator.jetPhi[igenJet], 0)
 
+        #reject very soft jets, usually poorly defined
         if Gen_jet.Pt()<15.: continue
-        #if Gen_jet.Pt()<87. or Gen_jet.Pt()>88.: continue
-        #print(Gen_jet.Pt())
 
         total.Fill(Gen_jet.Pt())
         pt.Fill(Gen_jet.Pt())
@@ -72,102 +81,75 @@ for i in range(0, nevents):
         matched = False
         highestL1Pt = -99.
 
+        #loop on L1 jets to find match
         for ijet in range(0, L1_nJets):
             L1_jet = ROOT.TLorentzVector()
             L1_jet.SetPtEtaPhiM(emuTree.L1Upgrade.jetEt[ijet], emuTree.L1Upgrade.jetEta[ijet], emuTree.L1Upgrade.jetPhi[ijet], 0)
 
+            #check matching
             if Gen_jet.DeltaR(L1_jet)<0.5:
                 matched = True
+                #keep only L1 match with highest pT
                 if L1_jet.Pt()>highestL1Pt:
                     highestL1Pt = L1_jet.Pt()
-                
-        if matched and highestL1Pt>20.: passing20.Fill(Gen_jet.Pt())
-        if matched and highestL1Pt>35.: passing35.Fill(Gen_jet.Pt())
-        if matched and highestL1Pt>50.: passing50.Fill(Gen_jet.Pt())
-        if matched and highestL1Pt>100.: passing100.Fill(Gen_jet.Pt())
-        if matched and highestL1Pt>150.: passing150.Fill(Gen_jet.Pt())
 
+        #fill numerator histograms for every thresholds
+        for i,ele in enumerate(thresholds): 
+            if matched and highestL1Pt>float(ele): passing[i].Fill(Gen_jet.Pt())
 
-        #print("after match: ",Gen_jet.Pt())
+#define TGraphAsymmErrors for efficiency turn-ons
+turnons = []
 
-#print(passing20.Integral())
-#print(total.Integral())
+for i,ele in enumerate(thresholds):
+        turnons.append(ROOT.TGraphAsymmErrors(passing[i], total, "cp"))
 
-fileout = ROOT.TFile("efficiency_graphs_"+label+".root","RECREATE")
-passing20.Write()
-passing35.Write()
-passing50.Write()
-passing100.Write()
-passing150.Write()
-
-total.Write()
-pt.Write()
-
-turnon20 = ROOT.TGraphAsymmErrors(passing20,total,"cp")
-turnon35 = ROOT.TGraphAsymmErrors(passing35,total,"cp")
-turnon50 = ROOT.TGraphAsymmErrors(passing50,total,"cp")
-turnon100 = ROOT.TGraphAsymmErrors(passing100,total,"cp")
-turnon150 = ROOT.TGraphAsymmErrors(passing150,total,"cp")
-
+#define canvas for plotting
 canvas = ROOT.TCanvas("c","c",800,800)
 canvas.SetGrid(10,10);
 
+#use dummy histogram to define style
 empty.GetXaxis().SetTitle("Gen jet p_{T} [GeV]")
-#empty.GetYaxis().SetTitle("Efficiency")
 empty.SetTitle("")
-#empty.SetMaximum(1.4)
 
 empty.GetXaxis().SetRangeUser(0.,250.);
 empty.GetYaxis().SetRangeUser(0.,1.3);
 
-#empty.GetXaxis().SetTitle("p_{T}^{offl.} [GeV]");
 empty.GetXaxis().SetTitleOffset(1.3);
 empty.GetYaxis().SetTitle("L1 Efficiency");
 empty.GetYaxis().SetTitleOffset(1.3);
 empty.SetTitle("");
 empty.SetStats(0);
 
+#use multigraph for plotting all turn-ons on the same canvas
 mg = ROOT.TMultiGraph("mg","")
 
-mg.Add(turnon20,"PE")
-mg.Add(turnon35,"PE")
-mg.Add(turnon50,"PE")
-mg.Add(turnon100,"PE")
-mg.Add(turnon150,"PE")
-
-turnon20.SetMarkerColor(ROOT.kBlack)
-turnon35.SetMarkerColor(ROOT.kRed)
-turnon50.SetMarkerColor(ROOT.kBlue)
-turnon100.SetMarkerColor(ROOT.kGreen)
-turnon150.SetMarkerColor(ROOT.kMagenta)
-
-turnon20.SetLineColor(ROOT.kBlack)
-turnon35.SetLineColor(ROOT.kRed)
-turnon50.SetLineColor(ROOT.kBlue)
-turnon100.SetLineColor(ROOT.kGreen)
-turnon150.SetLineColor(ROOT.kMagenta)
+for i,ele in enumerate(thresholds):
+    mg.Add(turnons[i],"PE")
+    turnons[i].SetMarkerColor(i+1)
+    turnons[i].SetLineColor(i+1)
 
 empty.Draw();
 mg.Draw()
-#mg.GetXaxis().SetTitle("Gen jet p_{T} [GeV]")
-#mg.GetYaxis().SetTitle("Efficiency")
-#mg.SetTitle("")
-#turnon20.Draw("APE")
 
 legend = ROOT.TLegend(0.15,0.75,0.48,0.88)
 legend.SetBorderSize(0)
-legend.AddEntry(turnon20,"p_{T}^{L1} > 20 GeV","LPE")
-legend.AddEntry(turnon35,"p_{T}^{L1} > 35 GeV","LPE")
-legend.AddEntry(turnon50,"p_{T}^{L1} > 50 GeV","LPE")
-legend.AddEntry(turnon100,"p_{T}^{L1} > 100 GeV","LPE")
-legend.AddEntry(turnon150,"p_{T}^{L1} > 150 GeV","LPE")
+for i,ele in enumerate(thresholds):
+    legend.AddEntry(turnons[i],"p_{T}^{L1} > "+str(thresholds[i])+" GeV","LPE")
+
 legend.Draw("same")
 
 canvas.SaveAs("turnOn_"+label+".pdf")
 canvas.SaveAs("turnOn_"+label+".root")
 
-turnon20.Write()
 print("--")
 
+#saving histograms and efficiencies in root file for later plotting if desired
+fileout = ROOT.TFile("efficiency_graphs_"+label+".root","RECREATE")
+for i,ele in enumerate(thresholds): 
+    passing[i].Write()
+    turnons[i].Write()
 
-    
+total.Write()
+pt.Write()
+
+fileout.Close()
