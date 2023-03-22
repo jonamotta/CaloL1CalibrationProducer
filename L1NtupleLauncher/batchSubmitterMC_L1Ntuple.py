4981dac76d4d60e2b38c826cbb0ffb75e6c5b2fd
Elena Vernazza <e.vernazza1@campus.unimib.it>
2023-03-22 14:14:26 +0100

    secondary input file to have cal info from RAW

--- L1NtupleLauncher/batchSubmitterMC_L1Ntuple.py
+++ L1NtupleLauncher/batchSubmitterMC_L1Ntuple.py
@@ -5,21 +5,36 @@ from optparse import OptionParser
 # Script to submit MC production
 # --------- L1Ntuples ---------
 '''
-python3 batchSubmitterMC_L1Ntuple.py --indir /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV/RAW \
---out /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV/L1Ntuples \
---maxEvents -1 --queue short --globalTag 126X_mcRun3_2023_forPU65_v1 --caloParams caloParams_2022_v0_6_cfi
+python3 batchSubmitterMC_L1Ntuple.py --indir /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X/AOD \
+--secondarydir /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X/RAW \
+--out /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X/L1Ntuples \
+--maxEvents -1 --queue short --globalTag 126X_mcRun3_2023_forPU65_v1 --caloParams caloParams_2022_v0_6_cfi --reco #(DONE) 1381
+python3 batchSubmitterMC_L1Ntuple.py --indir /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X_7000_10000/AOD \
+--secondarydir /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X_7000_10000/RAW \
+--out /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X_7000_10000/L1Ntuples \
+--maxEvents -1 --queue short --globalTag 124X_mcRun3_2022_realistic_postEE_v1 --caloParams caloParams_2022_v0_6_cfi --reco #(DONE) 2998/3000
+python3 batchSubmitterMC_L1Ntuple.py --indir /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X_10000_15000/AOD \
+--secondarydir /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X_10000_15000/RAW \
+--out /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X_10000_15000/L1Ntuples \
+--maxEvents -1 --queue short --globalTag 124X_mcRun3_2022_realistic_postEE_v1 --caloParams caloParams_2022_v0_6_cfi --reco
+python3 batchSubmitterMC_L1Ntuple.py --indir /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X_15000_20000/AOD \
+--secondarydir /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X_15000_20000/RAW \
+--out /data_CMS/cms/motta/CaloL1calibraton/PrivateMC/QCD_Pt30_500_TuneCP5_13p6TeV_124X_15000_20000/L1Ntuples \
+--maxEvents -1 --queue short --globalTag 124X_mcRun3_2022_realistic_postEE_v1 --caloParams caloParams_2022_v0_6_cfi --reco
 '''
 
 if __name__ == "__main__" :
 
     parser = OptionParser()    
-    parser.add_option("--indir",      dest="indir",     type=str,            default=None,                            help="Input folder name")
-    parser.add_option("--out",        dest="out",       type=str,            default=None,                            help="Output folder name")
-    parser.add_option("--maxEvents",  dest="maxEvents", type=int,            default=-1,                              help="Number of events per job")
-    parser.add_option("--queue",      dest="queue",     type=str,            default='long',                          help="long or short queue")
-    parser.add_option("--globalTag",  dest="globalTag", type=str,            default='126X_mcRun3_2023_forPU65_v1',   help="Which globalTag to use")
-    parser.add_option("--caloParams", dest="caloParams",type=str,            default='caloParams_2022_v0_6_cfi',      help="Which caloParams to use")
-    parser.add_option("--no_exec",    dest="no_exec",   action='store_true', default=False)
+    parser.add_option("--indir",        dest="indir",     type=str,            default=None,                            help="Input folder name (AOD)")
+    parser.add_option("--secondarydir", dest="secondarydir",   type=str,            default=None,                            help="Secondary dir folder name (RAW)")
+    parser.add_option("--out",          dest="out",       type=str,            default=None,                            help="Output folder name")
+    parser.add_option("--maxEvents",    dest="maxEvents", type=int,            default=-1,                              help="Number of events per job")
+    parser.add_option("--queue",        dest="queue",     type=str,            default='long',                          help="long or short queue")
+    parser.add_option("--globalTag",    dest="globalTag", type=str,            default='126X_mcRun3_2023_forPU65_v1',   help="Which globalTag to use")
+    parser.add_option("--caloParams",   dest="caloParams",type=str,            default='caloParams_2022_v0_6_cfi',      help="Which caloParams to use")
+    parser.add_option("--reco",         dest="reco",      action='store_true', default=False)
+    parser.add_option("--no_exec",      dest="no_exec",   action='store_true', default=False)
     (options, args) = parser.parse_args()
 
     os.system('mkdir -p '+options.out)
@@ -27,15 +42,31 @@ if __name__ == "__main__" :
     inRootNameList = glob.glob(options.indir+"/Ntuple_*.root")
     inRootNameList.sort()
 
+    skipped = 0
+    resubmitting = 0
+
     for inRootName in inRootNameList:
 
         idx = inRootName.split(".root")[0].split("Ntuple")[1].split("_")[1]
+        SecondaryRootName = options.secondarydir + '/Ntuple_' + str(idx) + '.root'
         outJobName  = options.out + '/job_' + str(idx) + '.sh'
         outLogName  = options.out + '/log_' + str(idx) + '.txt'
         outRootName = options.out + '/Ntuple_' + str(idx) + '.root'
 
-        cmsRun = "cmsRun L1Ntuple_cfg.py inputFiles=file:"+inRootName+" outputFile=file:"+outRootName
+        # if the outRootName already exists there is no need of resubmitting
+        # but files not correctly closed have to be resubmitted
+        if os.path.isfile(outRootName):
+            if len(os.popen('grep "Run 1, Event 2000," '+outLogName).read()) > 0:
+                # print("Skipping "+outRootName)
+                skipped = skipped + 1
+                continue
+
+        resubmitting = resubmitting + 1
+
+        cmsRun = "cmsRun L1Ntuple_cfg.py inputFiles=file:"+inRootName+" outputFile=file:"+outRootName+" secondaryInputFiles=file:"+SecondaryRootName
         cmsRun = cmsRun+" maxEvents="+str(options.maxEvents)+" globalTag="+options.globalTag+" caloParams="+options.caloParams
+        if options.reco:
+            cmsRun = cmsRun + " reco=1"
         cmsRun = cmsRun+" >& "+outLogName
 
         skimjob = open (outJobName, 'w')
@@ -53,3 +84,6 @@ if __name__ == "__main__" :
         command = ('/home/llr/cms/evernazza/t3submit -'+options.queue+' \'' + outJobName +"\'")
         print(command)
         if not options.no_exec: os.system (command)
+
+    print("skipped = ", skipped)
+    print("resubmitting = ", resubmitting)
